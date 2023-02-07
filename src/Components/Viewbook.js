b5f8dc4a4b3601fcb08386920b300d53c66ca8c1
Sathiyapramod <sathiyapramod22@gmail.com>
2023-02-07 16:16:11 +0530

    added view options

--- src/Components/Viewbook.js
+++ src/Components/Viewbook.js
@@ -3,16 +3,14 @@ import { useParams } from "react-router-dom";
 import { API } from "./general";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
 import { CardContent, Typography } from "@mui/material";
 
 function Viewbook() {
   const { bookid } = useParams();
   const [book, setBook] = useState({});
-
+  const navigate = useNavigate();
   useEffect(() => {
     fetch(`${API}/Books/${bookid}`, {
       method: "GET",
@@ -20,33 +18,37 @@ function Viewbook() {
       .then((response) => response.json())
       .then((result) => setBook(result));
   }, []);
-  return <ViewDetail book={book} />;
-}
-function ViewDetail({ book }) {
-  let bookdata = book;
-  const navigate = useNavigate();
   return (
-    <div className="d-flex flex-row justify-content-center align-items-center">
-      <Paper sx={{ width: 600, height: 400 }}>
-        <Card>
-          <CardContent align="left">
-            <Typography>Title: {book.title}</Typography>
-            <Typography>Author: {book.author}</Typography>
-            <Typography>Language :{book.language}</Typography>
-            <Typography>Number of Pages:{book.pages}</Typography>
-            <Typography>Year of Publishing: {book.year}</Typography>
-            <Typography>
-              URL Link:{" "}
-              <a href={book.link} target="_blank">
-                {book.link}
-              </a>
-            </Typography>
-            <Button variant="contained" onClick={() => navigate("/books")}>
-              Go back
-            </Button>
-          </CardContent>
-        </Card>
-      </Paper>
+    <div className="d-flex flex-row justify-content-center align-items-center p-3">
+      <Card sx={{ width: 600, height: 400 }} align="center">
+        <CardContent align="center">
+          <img src={book.imageLink} alt={""} width={100} height={100} title={book.title}/>
+          <Typography >
+            Title: <b>{book.title}</b>
+          </Typography>
+          <Typography>
+            Author: <b>{book.author}</b>
+          </Typography>
+          <Typography>Language :{book.language}</Typography>
+          <Typography>Country: {book.country}</Typography>
+          <Typography>Number of Pages:{book.pages}</Typography>
+          <Typography>Year of Publishing: {book.year}</Typography>
+          <Typography>
+            URL Link:{" "}
+            <a href={book.link} target="_blank" rel="noreferrer">
+              {book.link}
+            </a>
+          </Typography>
+          <br />
+          <Button
+            variant="contained"
+            onClick={() => navigate("/books")}
+            align="center"
+          >
+            Go back
+          </Button>
+        </CardContent>
+      </Card>
     </div>
   );
 }
