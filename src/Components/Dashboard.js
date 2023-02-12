dd19c871d066c67f24be70e8a1948e6b11aeded8
Sathiyapramod <sathiyapramod22@gmail.com>
2023-02-12 16:30:50 +0530

    added react-reveal

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -3,26 +3,35 @@ import ChartData from "./LineChart";
 import UsersTable from "./UsersTable";
 import NewBooks from "./NewBooks";
 import { Typography } from "@mui/material";
+import Fade from "react-reveal/Fade";
 
 export function Dashboard() {
   return (
     <div>
-      <div className="d-flex flex-column justify-content-center">
-        <Typography variant="h2" color={"inherit"}>Welcome to User Dashboard</Typography>
-        <Typography variant="overline">Admin Dashboard </Typography>
-      </div>
+      <Fade left>
+        <div className="d-flex flex-column justify-content-center">
+          <Typography variant="h2" color={"inherit"}>
+            Welcome to User Dashboard
+          </Typography>
+          <Typography variant="overline">Admin Dashboard </Typography>
+        </div>
+      </Fade>
       <br />
       <div className="d-flex flex-row justify-content-center align-items-center"></div>
-      <div className="d-flex flex-row justify-content-center align-items-center gap-5">
-        <ChartData />
-        <PieChartDesign />
-      </div>
-      <div className="d-flex flex-row justify-content-center align-items-center">
-        <div className="userstable">
-          <UsersTable />
+      <Fade right>
+        <div className="d-flex flex-row justify-content-center align-items-center gap-5">
+          <ChartData />
+          <PieChartDesign />
         </div>
-        <NewBooks />
-      </div>
+      </Fade>
+      <Fade bottom>
+        <div className="d-flex flex-row justify-content-center align-items-center">
+          <div className="userstable">
+            <UsersTable />
+          </div>
+          <NewBooks />
+        </div>
+      </Fade>
     </div>
   );
 }
